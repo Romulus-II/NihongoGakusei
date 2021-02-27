95c97e359f7f487d1f83591fce557cc62fc07901
gfragoso <gabe.fragoso@valpo.edu>
2021-02-27 11:17:03 -0800

    Started login system

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const session = require('express-session');
+//const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const serv = require('http').Server(app);
 
-app.get('/',function(req, res) {
-	res.sendFile(__dirname + '/client/index.html');
-});
-app.use('/client',express.static(__dirname + '/client'));
+// Set up view engine
+app.set('views', path.join(__dirname, 'client/views'));
+app.set('view engine','ejs');
+
+
+app.use('/client', express.static(__dirname + '/client'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// Do not display secret
+app.use(session({secret: "abdj348t01ignjfq0ifjdkaf", resave: false, saveUninitialized: true}))
+
+
+var routes = require('./routes/index.js');
+
+app.use('/', routes);
 
 serv.listen(5000);
 console.log("Server started.");
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+// Home page route
+router.get('/login',function(req, res) {
+	//res.sendFile(__dirname + '/client/views/index.html');
+  res.render('log-in');
+});
+
+router.post('/login-request', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  console.log('Username: ' + username);
+  console.log('Password: ' + password);
+
+  //req.session.user = user;
+
+  // We want to change this to be either success or failure, with a message
+  res.send({status: 'success'});
+});
+
+
+module.exports = router;
